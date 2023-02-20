a07ead0eca59d63a346432a6086b9cdb091c7ac4
Zaurtime <zaurteam@gmail.com>
2023-02-20 11:09:44 +0000

    Change name and add photo

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -10,7 +10,7 @@ function initMap() {
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [
-        { lat: 40.785091, lng: -73.968285 },
+        { lat: 53.273262, lng: -9.03215 },
         { lat: 41.084045, lng: -73.874245 },
         { lat: 40.754932, lng: -73.984016 }
     ];
